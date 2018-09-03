8c53b07fb93a7675dbe2a852dcdb58218c76af10
Rishikesh Jadhav <rishijadhav2792@gmail.com>
2018-09-03 16:39:20 +0530

    Added Logger and try catch on Main() method

---
+++ NitorOSS.Azure.ProvisionServiceBus/NitorOSS.Azure.Logger/Logger.cs
@@ -0,0 +1,27 @@
+﻿namespace NitorOSS.Azure.Logger
+{
+    using System;
+
+    public static class Logger
+    {
+        public static void LogMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void LogError(string error)
+        {
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Error Occurred: " + error);
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+        }
+
+        public static void LogException(Exception exception)
+        {
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Exception Occurred: " + exception.Message);
+            Console.WriteLine("Stack Trace: " + exception.StackTrace);
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+        }
+    }
+}
